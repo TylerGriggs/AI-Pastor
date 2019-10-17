7819253ccb22daa10ca7aa5c862e47e5c20b1d6b
Tyler A Griggs <griggsta@vcu.edu>
2019-10-16 21:56:22 -0400

    Model Traineed
    Dont;' know wky it's not updateding the model saves

--- word-rnn-tensorflow-master/train.py
+++ word-rnn-tensorflow-master/train.py
@@ -20,19 +20,19 @@ def main():
                        help='directory containing tensorboard logs')
     parser.add_argument('--save_dir', type=str, default='save',
                        help='directory to store checkpointed models')
-    parser.add_argument('--rnn_size', type=int, default=256,
+    parser.add_argument('--rnn_size', type=int, default=512,
                        help='size of RNN hidden state')
     parser.add_argument('--num_layers', type=int, default=2,
                        help='number of layers in the RNN')
     parser.add_argument('--model', type=str, default='lstm',
                        help='rnn, gru, or lstm')
-    parser.add_argument('--batch_size', type=int, default=50,
+    parser.add_argument('--batch_size', type=int, default=150,
                        help='minibatch size')
     parser.add_argument('--seq_length', type=int, default=25,
                        help='RNN sequence length')
-    parser.add_argument('--num_epochs', type=int, default=200,
+    parser.add_argument('--num_epochs', type=int, default=500,
                        help='number of epochs')
-    parser.add_argument('--save_every', type=int, default=250,
+    parser.add_argument('--save_every', type=int, default=15,
                        help='save frequency')
     parser.add_argument('--grad_clip', type=float, default=5.,
                        help='clip gradients at this value')
@@ -40,7 +40,7 @@ def main():
                        help='learning rate')
     parser.add_argument('--decay_rate', type=float, default=0.97,
                        help='decay rate for rmsprop')
-    parser.add_argument('--gpu_mem', type=float, default=0.666,
+    parser.add_argument('--gpu_mem', type=float, default=0.887,
                        help='%% of gpu memory to be allocated to this process. Default is 66.6%%')
     parser.add_argument('--init_from', type=str, default=None,
                        help="""continue training from saved model at this path. Path must contain files saved by previous training process:
